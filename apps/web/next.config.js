610744cd97af53f1b518ffb1a5f4fd5c6ea5eb86
Stefano Frontini <stefanofrontini75@gmail.com>
2025-05-29 11:24:07 +0200

    azure deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,22 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        canvas: "./empty-module.ts",
-      },
+  outputFileTracingRoot: path.join(__dirname, "../../"),
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  turbopack: {
+    resolveAlias: {
+      canvas: "./empty-module.ts",
     },
   },
 };
